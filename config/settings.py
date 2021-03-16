c91c9dd800e5ccf68c3767e8cbcd68dd295a828c
kinoshita <kinoshitaryuta@gmail.com>
2021-03-16 14:47:49 +0900

    完成

--- config/settings.py
+++ config/settings.py
@@ -140,10 +140,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-# STATIC_URL = '/static/'
-# STATICFILES_DIRS=[os.path.join(BASE_DIR,'static')]
-#
-# MEDIA_URL = '/media/'
+STATIC_URL = '/static/'
+STATICFILES_DIRS=[os.path.join(BASE_DIR,'static')]
+MEDIA_URL = '/media/'
 MEDIA_ROOT = 'user/share/nginx/html/static'
 STATIC_ROOT='user/share/nginx/html/media'
 # Default logging for Django. This sends an email to the site admins on every
